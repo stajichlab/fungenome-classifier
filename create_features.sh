ed00e21643fcc3443b25d45dfa6930c2a7d89278
Jason Stajich <jasonstajich.phd@gmail.com>
2026-04-21 22:07:11 -0700

    create feature file

---
+++ create_features.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash -l
+#SBATCH -c 64 --mem 128gb -p exfab --out logs/make_features.log
+
+module load cuda/12.8 cudnn
+CPU=1
+if [ ! -z $SLURM_CPUS_ON_NODE ]; then
+	CPU=$SLURM_CPUS_ON_NODE
+fi
+
+~/.pixi/bin/pixi run make features N_JOBS=$CPU
